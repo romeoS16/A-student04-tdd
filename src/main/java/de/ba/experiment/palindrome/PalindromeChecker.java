0b43a57d121e356ab89ea05507e94b5701f780f1
Friederike <friederike.koerte@gmail.com>
2025-12-18 12:25:29 +0100

    Aufgabe 2

--- src/main/java/de/ba/experiment/palindrome/PalindromeChecker.java
+++ src/main/java/de/ba/experiment/palindrome/PalindromeChecker.java
@@ -1,5 +1,7 @@
 package de.ba.experiment.palindrome;
 
+import java.util.Locale;
+
 public class PalindromeChecker {
 
     /**
@@ -15,6 +17,21 @@ public class PalindromeChecker {
 
     public static boolean isPalindrome(String text) {
         // TODO: implementieren
-        return false;
+
+        if (text == null){
+            return false;
+        }
+
+        text = text.toLowerCase(Locale.ROOT);
+        text = text.replaceAll("\\s", "");
+
+        String reversed = "";
+        for (int i = text.length()-1; i>=0; i--){
+            reversed += text.charAt(i);
+        }
+        return text.equals(reversed);
     }
+
+
+
 }
--- src/test/java/de/ba/experiment/palindrome/PalindromeCheckerTest.java
+++ src/test/java/de/ba/experiment/palindrome/PalindromeCheckerTest.java
@@ -3,6 +3,7 @@ package de.ba.experiment.palindrome;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class PalindromeCheckerTest {
 
@@ -17,4 +18,36 @@ public class PalindromeCheckerTest {
     }
 
     // TODO: weitere Testfälle ergänzen
+
+    @Test
+    public void noPalindrome_returnsFalse(){
+        boolean result = PalindromeChecker.isPalindrome("romeo");
+        assertFalse(result);
+    }
+
+    @Test
+    public void upperLowercase() {
+        boolean result = PalindromeChecker.isPalindrome("Anna");
+        assertTrue(result);
+    }
+
+    @Test
+    public void space() {
+        boolean result = PalindromeChecker.isPalindrome("a abaa");
+        assertTrue(result);
+    }
+
+    @Test
+    public void emptyString(){
+        boolean result = PalindromeChecker.isPalindrome("");
+        assertTrue(result);
+    }
+
+    @Test
+    public void nullString(){
+        boolean result = PalindromeChecker.isPalindrome(null);
+        assertFalse(result);
+    }
+
+
 }
